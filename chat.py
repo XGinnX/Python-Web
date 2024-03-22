b2388c7ba12cb7906e3f9ac98b9dc6fcb8d2f54e
Jean Sales <jean.sls.snts@gmail.com>
2024-03-21 22:57:02 -0300

    Imagens

---
+++ chat.py
@@ -0,0 +1,51 @@
+# Titulo: ChatZap
+# 1. Botão de Iniciar
+    # 1.1 Abre Pop Up/ Modal ao clicar no botão
+        # 1.1.1 Modal deve possuir
+         # Titulo: Bem vindo ao ChatZap
+         # campo: Escreva seu nome no chatZap
+         # botão: Entre no chat
+# 2. Chat
+ #2.1. Espaço CHat de mensagens
+ #2.2. A Baixo do espaço de chat deve possuir:
+       # Campo Digite a sua mensagem
+       # botão de enviar
+       
+# Passo a passo para utilizar o FLET
+       
+import flet as ft # 1. Importar
+
+def main (pagina): # 2. criar função principal
+    texto = ft.Text("ChatZap")
+    
+    def entrar_no_chat(evento):
+         print("Entrar no chat")
+         pagina.remove(texto)
+         pagina.remove(botao_iniciar)
+         pagina.update()
+    
+    titulo_popup = ft.Text("Bem vindo ao ChatZap")
+    nome_usuario = ft.TextField(label="Escreva seu nome no chat")
+    botao_entrar = ft.ElevatedButton("Entrar no chat", on_click=entrar_no_chat)
+    
+    popup = ft.AlertDialog(
+       open=False,
+       modal=True,
+       title=titulo_popup,
+       content=nome_usuario,
+       actions=[botao_entrar]
+    )
+    
+    def abrir_popup(evento):
+     print("Abrir o chat")
+     pagina.dialog = popup
+     popup.open = True
+     pagina.update()
+     
+    
+    botao_iniciar = ft.ElevatedButton("Iniciar Chat", on_click=abrir_popup)    
+    
+    pagina.add(texto)
+    pagina.add(botao_iniciar)
+    
+ft.app(target=main) # criar o app chamando a função principal
---
+++ readme.md
@@ -0,0 +1,14 @@
+### utilizamos o FLET
+Utilizamos o flet por poder ser reutilizado.
+
+outros frameworks:
+- Django
+- Flask
+
+flamework - São conjunto de ferramentas
+
+### utilizando flat
+
+1. importar o flet
+2. criar a função principal - main
+3. chama a função principal
